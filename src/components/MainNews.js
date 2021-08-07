4048ac0df8e4492e1a2261d2b3c24ca5c16ae74c
Cristopher-91 <k.piasecki91@gmail.com>
2021-08-07 18:48:38 +0200

    Added basic React-Router

---
+++ src/components/MainNews.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react'
+
+import SectionAbout from './SectionAbout'
+import SectionContact from './SectionContact'
+import SectionNews from './SectionNews'
+import SectionShop from './SectionShop'
+
+
+function MainNews() {
+    return (
+        <div>
+            <p>Personel</p>
+        </div>
+    )
+}
+
+export default MainNews
---
+++ src/pages/Staff.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Header'
+import MainNews from '../components/MainNews'
+import Footer from '../components/Footer'
+
+function Home() {
+    return (
+        <>
+            <Header/>
+            <MainNews/>
+            <Footer/>
+        </>
+    )
+}
+
+export default Home
