af9e064a2e15bb14afeb78d20b051e3f269b2dd1
Cristopher-91 <k.piasecki91@gmail.com>
2021-08-12 14:05:32 +0200

    Working on given example

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Nav, NavDropdown } from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown, Toggle, Collapse, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
 import { AiOutlineHome } from 'react-icons/ai'
 
@@ -7,24 +7,41 @@ import LogoFlattImage from '../img/Red-Vet-Logo-Flatt.png'
 
 function Menu() {
 
+
+    const [mobileToggle, setmobileMenu] = useState(false);
+
+
     document.addEventListener('DOMContentLoaded', function () {
 
-        let navbar = document.getElementById("nav-main");
-        let navLogo = document.getElementById("nav-logo");
 
-        let change = navbar.offsetTop;
 
+
+
+        //let navbar = document.getElementById("nav-main");
+        //let navLogo = document.getElementById("nav-logo");
+
+        //let change = navbar.offsetTop;
+
+        var navbar = document.getElementById('nav-wrapper');
+        //let scroll = navbar.offsetTop;
 
         const toggleVisible = () => {
-            if (window.pageYOffset > change) {
-                navbar.classList.add("navbar-white");
-                navLogo.classList.remove("navbar-brand-logo-white");
+            // if (window.pageYOffset > change) {
+            //     navbar.classList.add("navbar-white");
+            //     navLogo.classList.remove("navbar-brand-logo-white");
 
-            } else {
-                navbar.classList.remove("navbar-white");
-                navLogo.classList.add("navbar-brand-logo-white");
-            }
+            // } else {
+            //     navbar.classList.remove("navbar-white");
+            //     navLogo.classList.add("navbar-brand-logo-white");
+            // }
+
+            // if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+            //     navbar.classList.remove('start-style').classList.add("scroll-on");
+            // } else {
+            //     navbar.classList.remove("scroll-on").classList.add('start-style');
+            // }
         }
+
 
 
         window.addEventListener('scroll', toggleVisible);
@@ -32,7 +49,7 @@ function Menu() {
 
     const [showDropdownMenu, setshowDropdown] = useState(false)
 
-    const showDropdown = (e)=>{
+    const showDropdown = (e) => {
         setshowDropdown(!showDropdownMenu);
     }
     const hideDropdown = e => {
@@ -40,61 +57,116 @@ function Menu() {
     }
 
     return (
-        <Nav
-            className="navbar fixed-top navbar-expand-lg"
-            id="nav-main">
-            <div className='container-fluid'>
-                <Link
-                    className="navbar-brand"
-                    to={"/"}>
+        // <div className='nav-wrapper fixed-top nav-start' id='nav-wrapper'>
+        //     <div className='container-fluid'>
+        //         <Nav
+        //             className="navbar navbar-expand-md"
+        //             id="nav-main">
+        //             <Navbar.Brand href="/">
+        //                 <img
+        //                     src={LogoFlattImage}
+        //                     alt="Logo Przychodni Red-Vet"
+        //                     className="navbar-brand-logo"
+        //                     id="nav-logo" />
+        //             </Navbar.Brand>
+        //             <button 
+        //                 className="navbar-toggler" 
+        //                 type="button" data-toggle="collapse" 
+        //                 data-target="#navbarNav" 
+        //                 aria-controls="navbarNav" 
+        //                 aria-expanded="false" 
+        //                 aria-label="Toggle navigation">
+        // 					<span className="navbar-toggler-icon"></span>
+        // 			</button>
+        //             <div 
+        //                 className=" collapse navbar-collapse justify-content-end"
+        //                 id="navbarNav">
+
+        //                 <ul className="navbar-nav mr-auto">
+        //                     {/* <Nav.Item as='li'>
+        //                         <Nav.Link classname='nav-link' href={'/'} className="nav-link"><AiOutlineHome /></Nav.Link >
+        //                     </Nav.Item> */}
+                            // <NavDropdown
+                            //     title='O nas'
+                            //     id='nav-dropdown'>
+                            //     {/*show={showDropdownMenu}*/}
+                            //     {/*} onMouseEnter={showDropdown}*/}
+                            //     {/*} onMouseLeave={hideDropdown}*/}
+                            //     <Nav.Item as='li'>
+                            //         <Nav.Link classname='nav-link' href={'/personel'} className="nav-link">Personel</Nav.Link >
+                            //     </Nav.Item>
+                            //     <Nav.Item as='li'>
+                            //         <Nav.Link classname='nav-link' href={'/przychodnia'} className="nav-link">Przychodnia</Nav.Link >
+                            //     </Nav.Item>
+                            // </NavDropdown>
+                            // <Nav.Item as='li'>
+                            //     <Nav.Link classname='nav-link' href={'/przychodnia'} className="nav-link">Usługi</Nav.Link >
+                            // </Nav.Item>
+                            // <Nav.Item as='li'>
+                            //     <a classname='nav-link' href={'#sklep'} className="nav-link">Sklep</a >
+                            // </Nav.Item>
+                            // <Nav.Item as='li'>
+                            //     <Nav.Link classname='nav-link' href={'/aktualnosci'} className="nav-link">Aktualności</Nav.Link >
+                            // </Nav.Item>
+                            // <Nav.Item as='li'>
+                            //     <Nav.Link classname='nav-link' href={'#opinie'} className="nav-link">Opinie</Nav.Link >
+                            // </Nav.Item>
+                            // <Nav.Item as='li'>
+                            //     <Nav.Link classname='nav-link' href={'#kontakt'} className="nav-link">Kontakt</Nav.Link >
+                            // </Nav.Item>
+
+
+        //                 </ul>
+        //             </div>
+        //         </Nav>
+        //     </div>
+        // </div>
+        <div className='nav-wrapper start-header start-style'>
+        <Navbar expand="md">
+            <Container fluid>
+                <Navbar.Brand href="/">
                     <img
                         src={LogoFlattImage}
                         alt="Logo Przychodni Red-Vet"
-                        className="navbar-brand-logo navbar-brand-logo-white"
+                        className="navbar-brand-logo"
                         id="nav-logo" />
-                </Link>
-                <div
-                    class="collapse navbar-collapse justify-content-end"
-                    id="navbarNav">
-
-                    <ul className="navbar-nav mr-auto">
-                        <Nav.Item as='li'>
-                            <Nav.Link classname='nav-link' href={'/'} className="nav-link"><AiOutlineHome /></Nav.Link >
-                        </Nav.Item>
-                        <NavDropdown 
-                            title='O nas' 
-                            id='nav-dropdown'
-                            show={showDropdownMenu}
-                            onMouseEnter={showDropdown} 
-                            onMouseLeave={hideDropdown}>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={ mobileToggle }/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                    <NavDropdown
+                                title='O nas'
+                                id='nav-dropdown'>
+                                {/*show={showDropdownMenu}*/}
+                                {/*} onMouseEnter={showDropdown}*/}
+                                {/*} onMouseLeave={hideDropdown}*/}
+                                <Nav.Item as='li'>
+                                    <Nav.Link classname='nav-link' href={'/personel'} className="nav-link">Personel</Nav.Link >
+                                </Nav.Item>
+                                <Nav.Item as='li'>
+                                    <Nav.Link classname='nav-link' href={'/przychodnia'} className="nav-link">Przychodnia</Nav.Link >
+                                </Nav.Item>
+                            </NavDropdown>
                             <Nav.Item as='li'>
-                                <Nav.Link classname='nav-link' href={'/personel'} className="nav-link">Personel</Nav.Link >
-                            </Nav.Item>
-                            <Nav.Item as='li'>
-                                <Nav.Link classname='nav-link' href={'/przychodnia'} className="nav-link">Przychodnia</Nav.Link >
-                            </Nav.Item>
-                        </NavDropdown>
-                        <Nav.Item as='li'>
                                 <Nav.Link classname='nav-link' href={'/przychodnia'} className="nav-link">Usługi</Nav.Link >
                             </Nav.Item>
-                        <Nav.Item as='li'>
-                            <a classname='nav-link' href={'#sklep'} className="nav-link">Sklep</a >
-                        </Nav.Item>
-                        <Nav.Item as='li'>
-                            <Nav.Link classname='nav-link' href={'/aktualnosci'} className="nav-link">Aktualności</Nav.Link >
-                        </Nav.Item>
-                        <Nav.Item as='li'>
-                            <Nav.Link classname='nav-link' href={'#opinie'} className="nav-link">Opinie</Nav.Link >
-                        </Nav.Item>
-                        <Nav.Item as='li'>
-                            <Nav.Link classname='nav-link' href={'#kontakt'} className="nav-link">Kontakt</Nav.Link >
-                        </Nav.Item>
-
-
-                    </ul>
-                </div>
-            </div>
-        </Nav>
+                            <Nav.Item as='li'>
+                                <a classname='nav-link' href={'#sklep'} className="nav-link">Sklep</a >
+                            </Nav.Item>
+                            <Nav.Item as='li'>
+                                <Nav.Link classname='nav-link' href={'/aktualnosci'} className="nav-link">Aktualności</Nav.Link >
+                            </Nav.Item>
+                            <Nav.Item as='li'>
+                                <Nav.Link classname='nav-link' href={'#opinie'} className="nav-link">Opinie</Nav.Link >
+                            </Nav.Item>
+                            <Nav.Item as='li'>
+                                <Nav.Link classname='nav-link' href={'#kontakt'} className="nav-link">Kontakt</Nav.Link >
+                            </Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        </div>
     )
 }
 
