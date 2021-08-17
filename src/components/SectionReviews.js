6b667c29d70a9c3ffa5f39d8681f9d45dd1a992d
Cristopher-91 <k.piasecki91@gmail.com>
2021-08-17 13:25:27 +0200

    Finished reviews carousel

--- src/components/SectionReviews.js
+++ src/components/SectionReviews.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
+import ReviewsCarousel from './ReviewsCarousel'
 import Section from './Section'
 
 function SectionReviews() {
     return (
         <Section id='opinie' name='reviews' paralax='true'>
             <Row>
-                <Col sm={5} className='paralax-content'>
+                <Col sm={6} className='paralax-content'>
                     <h2 className="paragraph-title-green">Opinie</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent at maximus lectus. Suspendisse a sapien sit amet ex ultricies tempus nec sit amet nulla. Proin sapien augue, egestas vel arcu a, euismod consequat justo</p>
+                    <ReviewsCarousel/>
                 </Col>
             </Row>
         </Section>
