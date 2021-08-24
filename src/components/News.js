fd7d484a9b4a888fefdeb137bf2fcb6cb308f706
Cristopher-91 <k.piasecki91@gmail.com>
2021-08-24 11:00:41 +0200

    Changed news button and padding

--- src/components/News.js
+++ src/components/News.js
@@ -4,21 +4,35 @@ import { Card, Button } from 'react-bootstrap'
 import image from '../img/Red-Vet-Logo.png'
 
 function News(props) {
+
+
+
+    // function truncateText() {
+    //     var length = 150;
+    //     var fullString =  props.text;
+    //     alert(fullString);
+    //     var truncString = fullString.substring(0,length);
+
+    //     return truncString;
+    // }
+
     return (
         <div className='mb-4 col-lg-3 col-md-4 col-sm-6'>
-        <Card className='news-card' >
-            <Card.Img className='card-image' src={image} />
-            <Card.Body>
-                <Card.Title>{props.title}</Card.Title>
-                <p className='card-date'>{props.date}</p>
-                <Card.Text>
-                    {/* Some quick example text to build on the card title and make up the bulk of
+            <Card className='news-card' >
+                <Card.Img className='card-image' src={image} />
+                <Card.Body>
+                    <Card.Title>{props.title}</Card.Title>
+                    <p className='card-date'>{props.date}</p>
+                    <Card.Text>
+                        {/* Some quick example text to build on the card title and make up the bulk of
                     the card's content. Lorem ipsum notum datum ergo ibsum pero cratum. Viven croto nat peroxis iber catum mixum voxis. */}
-                    {props.children}
-                </Card.Text>
-                <button className='button-small'>Czytaj więcej</button>
-            </Card.Body>
-        </Card>
+                        {props.text}
+                    </Card.Text>
+                    <div className='card-read-more'>
+                        <a href='#'>Czytaj więcej {'>'}</a>
+                    </div>
+                </Card.Body>
+            </Card>
         </div>
 
     )
