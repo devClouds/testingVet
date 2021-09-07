b447d6d73d50be3a57abafa3caddbf2b78d45cce
Cristopher-91 <k.piasecki91@gmail.com>
2021-09-07 10:09:13 +0200

    Fixed a paths to NavLinks + Firebase deploy

--- src/components/SectionNews.js
+++ src/components/SectionNews.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Col, Row } from 'react-bootstrap'
 import Section from './Section'
 import SectionTitle from './SectionTitle'
+import { NavLink } from 'react-router-dom';
 
 import Fade from 'react-reveal/Fade'
 
@@ -14,7 +15,7 @@ function SectionNews() {
                     <Fade bottom>
                         <p>Chcesz dowiedzieć się co u nas nowego? Upewnić się czy jesteśmy dzisiaj otwarci. Poznać zwierzaki, które się u nas pojawiają? Przeczytać nieco ciekawych informacji ze świata weterynarii oraz dbania o zwierzęta. To wszystko oraz znacznie więcej odnajdziesz w tej sekcji</p>
                         <p>Staramy się na bieżąco publikować wszystkie istotne informacje dlatego zachęcamy do zaglądania do sekcji aktualności, by niczego nie przegapić.</p>
-                        <a className='button-small mt-3' href={'/aktualnosci'} >Przejdź do aktualności</a >
+                        <NavLink className='button-small mt-3' to={'/aktualnosci'} >Przejdź do aktualności</NavLink >
                     </Fade>
                 </Col>
             </Row>
--- src/components/SectionShop.js
+++ src/components/SectionShop.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Section from './Section'
 import { Col, Row } from 'react-bootstrap'
 import SectionTitle from './SectionTitle'
+import { NavLink } from 'react-router-dom';
 
 import Fade from 'react-reveal/Fade'
 
@@ -14,7 +15,7 @@ function SectionShop() {
                     <Fade bottom >
                     <p>Osoby odwiedzające naszą przychodnią mogą nie tylko skorzystać z szerokiego zakresu naszych usług medycznych, ale w międzyczasie odwiedzić nasz sklep z bogatym asortymentem produktów dla zwierząt domowych.</p>
                     <p>Nie wychodząc z przychodni, wygodnie i szybko nabędą Państwo zarówno wysokojakościową karmę znanych producentów, słodkie przysmaki, środki higieny zwierzęcej, a także zabawki dla swoich milusińskich -  wszystko zaś pod okiem i za radą naszego personelu. Serdecznie zapraszamy </p>
-                    <a className='button-small mt-3' href={'/sklep'} >Przejdź do sklepu</a >
+                    <NavLink className='button-small mt-3' to={'/sklep'} >Przejdź do sklepu</NavLink >
                     </Fade>
                 </Col>
             </Row>
