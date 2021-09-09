4dd02935d08416ed3d42a4fee6ad5a5a5ffc5e4e
Cristopher-91 <k.piasecki91@gmail.com>
2021-09-09 11:09:25 +0200

    Changed a hrefs in About.js to NavLinks

--- src/components/Home/Sections/About.js
+++ src/components/Home/Sections/About.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
 import Section from '../../Section'
+import SectionTitle from '../../SectionTitle'
 
 import buildingImage from '../../../img/Building.jpg'
 import staffImage from '../../../img/Staff.jpg'
-import SectionTitle from '../../SectionTitle'
+
+import { NavLink } from 'react-router-dom';
 
 import Bounce from 'react-reveal/Bounce';
 import Fade from 'react-reveal/Fade'
@@ -24,11 +26,11 @@ function About() {
                 <Col md={6} className='text-center text-md-start' >
                     <Bounce left>
                         <div>
-                            <a className='h3-link' href='/przychodnia'>
+                            <NavLink className='h3-link' to={'/przychodnia'}>
                                 <img className='img-size' src={buildingImage} alt='Widok frontu przychodni' />
                                 <h3 className='text-center text-md-start'>Przychodnia</h3>
                                 <p>W dobrych warunkach łatwiej dochodzić do zdrowia - zapraszamy zatem do krótkiej wycieczki po naszej przychodni w trakcie, której zapoznacie się z jej historią, działalnością oraz samym wyglądem. Ważne dla nas jest byście wiedzieli Państwo do jakiego miejsca oddajecie swych pupili.</p>
-                            </a>
+                            </NavLink>
                         </div>
 
                     </Bounce >
@@ -36,11 +38,11 @@ function About() {
                 <Col md={6} className='text-center text-md-start' >
                     <Bounce right>
                         <div>
-                            <a className='h3-link' href='/personel'>
+                            <NavLink  className='h3-link' to={'/personel'}>
                                 <img className='img-size' src={staffImage} alt='Zdjęcie przedstawiające Annę Redlicką - właścicielkę' />
                                 <h3 className='text-center text-md-start'>Personel</h3>
                                 <p>W niesieniu pomocy dostęp do dobrego sprzętu medycznego jest niezwykle istotny - nie ulega jednak wątpliwości, iż w ostatecznym rozrachunku to człowiek pełni kluczową funkcję w procesie leczenia weterynaryjnego. Zachęcamy więc do zapoznania się z naszym wykwalifikowanym personelem</p>
-                            </a>
+                            </NavLink >
                         </div>
                     </Bounce >
                 </Col>
